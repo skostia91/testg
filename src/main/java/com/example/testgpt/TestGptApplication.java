a3733457fc03a70f880c24ea29f7228959c6d35f
Костя <@https://github.com/skostia91/git-test>
2023-11-27 14:40:40 +0300

    1

---
+++ src/main/java/com/example/testgpt/TestGptApplication.java
@@ -0,0 +1,13 @@
+package com.example.testgpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestGptApplication.class, args);
+    }
+
+}
